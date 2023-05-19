0e43dac300e39d5159d217c879a20e34241cd715
jwanali <ciwan_ali@hotmail.com>
2023-05-18 19:44:39 -0500

    separating connect function and import using ES6

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
-const net = require("net");
 
+const {connect} = require("./client");
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost" ,
-    port: "50541",
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
- /* conn.on("connect", () => {
-    conn.write("Hello from client!");
-  });
-  */
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
