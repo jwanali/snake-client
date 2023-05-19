bd0e772a454dbf26bdcad7ee56863f4d510d80ce
jwanali <ciwan_ali@hotmail.com>
2023-05-19 00:53:41 -0500

    add move

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+//const { setTimeout } = require("timers/promises");
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost" ,
@@ -12,12 +13,35 @@ const connect = function () {
   });
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-  });
-  conn.on("connect", () => {
-    conn.write("Name: JAL");
-  })
- 
+    //console.log('"Move: up" - move up one square (unless facing down)');
+    //conn.write("Move: up");
+    conn.write("Name: J.L");
 
+
+    
+  });
+  
+  conn.on("connect", (data) => {
+    /*
+    setInterval(() => {
+      conn.write("Move: down");
+    },400);
+    
+    setInterval(() => {
+      conn.write("Move: left");
+    },50);
+    setInterval(() => {
+      conn.write("Move: up");
+    },50);
+    setInterval(() => {
+      conn.write("Move: right");
+    },50)
+   clearInterval;
+   */
+  });
+  
+  
+   
   return conn;
 };
 module.exports = {
--- play.js
+++ play.js
@@ -1,7 +1,13 @@
 
 const {connect} = require("./client");
 // establishes a connection with the game server
-
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
