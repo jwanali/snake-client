e84583751d0d028fd2dd7690b68c58afb6c64513
jwanali <ciwan_ali@hotmail.com>
2023-05-19 23:02:07 -0500

    update

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
 
 const {connect} = require("./client");
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = function () {
-  
-  if ( setupInput === '\u0003') {
-    process.exit();
-  }
-};
+const {setupInput} = require("./input")
 
-const stdin = process.stdin;
 
-stdin.on("data", handleUserInput);
+
 
 console.log("Connecting ...");
 connect();
+setupInput();
